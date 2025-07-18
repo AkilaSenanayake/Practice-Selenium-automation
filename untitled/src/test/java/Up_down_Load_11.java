065fb14276691c52e72b3b8ea3490adecdd5a02a
Akila Senanayake <akilasenanayake22@gmail.com>
2025-07-18 13:32:20 +0530

    Upload and download document automation

--- untitled/src/test/java/Up_down_Load_11.java
+++ untitled/src/test/java/Up_down_Load_11.java
@@ -1,10 +1,15 @@
+import com.sun.jna.platform.unix.X11;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 import java.io.File;
 
 public class Up_down_Load_11 {
@@ -37,7 +42,62 @@ public class Up_down_Load_11 {
     }
 
     @Test
-    public void uploadTest(){
+    public void uploadTest1() throws AWTException, InterruptedException {
 
+        //C:\\Users\\akila\\Downloads\\TestLeaf Logo.png
+        //1st way - Using Robot Class
+
+        WebElement uploadBtn = driver.findElement(By.id("j_idt88:j_idt89"));
+        uploadBtn.click();
+
+        //windows control begin here
+
+        String data = "C:\\Users\\akila\\Downloads\\TestLeaf Logo.png";
+        StringSelection selection = new StringSelection(data);
+
+        //coping path to clipboard
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection,null);
+
+        Robot robot = new Robot();
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+
+        Thread.sleep(3000);  //Thread is use only to see the automation when practice
+
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+    }
+
+
+    @Test
+    public void uploadTest2(){
+
+        //2nd way - Using send keys (Applicable only element type is file)
+
+        String data = "C:\\Users\\akila\\Downloads\\TestLeaf Logo.png";
+        WebElement uploadUsingSendKeys = driver.findElement(By.id("j_idt88:j_idt89_input"));
+        uploadUsingSendKeys.sendKeys(data);
+    }
+
+
+    @AfterMethod
+    public void close(){
+        driver.quit();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
