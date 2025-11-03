09024e1e66f9a9ceaafcc405e53c36e3133cf253
Akila Senanayake <akilasenanayake22@gmail.com>
2025-11-03 15:37:41 +0530

    Enhance TestNG configuration for parallel execution and add annotation methods for improved test structure

--- untitled/src/test/java/testNG/AnnotationHierarchyTest.java
+++ untitled/src/test/java/testNG/AnnotationHierarchyTest.java
@@ -1,7 +1,58 @@
 package testNG;
 
+import org.testng.annotations.*;
+
 public class AnnotationHierarchyTest {
-    public void Test(){
-        System.out.println("Beforemeth");
+
+
+    @Test
+    public void Test1(){
+        System.out.println("Test 1");
     }
+
+    @Test
+    public void Test2(){
+        System.out.println("Test 2");
+    }
+
+    @BeforeMethod
+    public void BeforeMethod() {
+        System.out.println("Before Method");
+    }
+
+    @AfterMethod
+    public void AfterMethod() {
+        System.out.println("After Method");
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("Before class");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("After class");
+    }
+
+    @BeforeTest
+    public void BeforeTest(){
+        System.out.println("Before Test");
+    }
+
+    @AfterTest
+    public void AfterTest(){
+        System.out.println("After Test");
+    }
+
+    @BeforeSuite
+    public void BeforeSuite(){
+        System.out.println("Before Suite");
+    }
+
+    @AfterSuite
+    public void AfterSuite(){
+        System.out.println("After Suite");
+    }
+
 }
