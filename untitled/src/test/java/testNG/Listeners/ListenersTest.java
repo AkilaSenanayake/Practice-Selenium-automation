20f99e64c8a2ce76ff65961ad1f65b8e3ea23180
Akila Senanayake <akilasenanayake22@gmail.com>
2025-11-04 11:02:44 +0530

    Add custom TestNG listeners and update XML configuration for test execution

--- untitled/src/test/java/testNG/Listeners/ListenersTest.java
+++ untitled/src/test/java/testNG/Listeners/ListenersTest.java
@@ -1,2 +1,31 @@
-package testNG.Listeners;public class ListenersTest {
+package testNG.Listeners;
+
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(CustomListeners.class)  //way 1 (can use multiple classes with { } this)
+public class ListenersTest {
+
+    @Test
+    public void test1(){
+        System.out.println("I am inside testMethod1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("I am inside testMethod2");
+        Assert.assertTrue(false);  //to fail the test case
+    }
+
+    @Test(timeOut = 1000)
+    public void test3() throws InterruptedException {
+        Thread.sleep(2000);
+        System.out.println("I am inside testMethod3");  //to timeout test case
+    }
+
+    @Test(dependsOnMethods = "test3")
+    public void test4(){
+        System.out.println("I am inside testMethod4");  //to skip the test case
+    }
 }
--- untitled/src/test/java/testNG/Listeners/ListenersWay2.java
+++ untitled/src/test/java/testNG/Listeners/ListenersWay2.java
@@ -1,2 +1,30 @@
-package testNG.Listeners;public class ListenersWay2 {
+package testNG.Listeners;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+//Create XML file
+public class ListenersWay2 {
+
+    @Test
+    public void test1(){
+        System.out.println("I am inside testMethod1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("I am inside testMethod2");
+        Assert.assertTrue(false);  //to fail the test case
+    }
+
+    @Test(timeOut = 1000)
+    public void test3() throws InterruptedException {
+        Thread.sleep(2000);
+        System.out.println("I am inside testMethod3");  //to timeout test case
+    }
+
+    @Test(dependsOnMethods = "test3")
+    public void test4(){
+        System.out.println("I am inside testMethod4");  //to skip the test case
+    }
 }
